40337d4b9be5c0905a376b8b8a61c2c44de83887
icessspark <shenyicong1023@163.com>
2020-05-08 18:21:44 +0800

    update example for douban_music, douban_book

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 from scrapy.cmdline import execute
 
 # scrapy crawl moviesearch
-execute(
-    [
-        "scrapy",
-        "crawl",
-        "moviesearch",
-        "-akeyword=诺兰",
-        "-apage=1",
-        "-sMONGO_URL=mongodb://localhost:27017/",
-        "-sSPIDER_NAME=moviesearch",
-        "-sTASK_ID=001",
-        "-sJOB_ID=002",
-    ]
-)
+# execute(
+#     [
+#         "scrapy",
+#         "crawl",
+#         "moviesearch",
+#         "-akeyword=诺兰",
+#         "-apage=1",
+#         "-sMONGO_URL=mongodb://localhost:27017/",
+#         "-sSPIDER_NAME=moviesearch",
+#         "-sTASK_ID=001",
+#         "-sJOB_ID=002",
+#     ]
+# )
 
 # scrapy crawl movietop250
 # execute(
@@ -44,16 +44,16 @@ execute(
 # )
 
 # scrapy crawl booksearch
-# execute(
-#     [
-#         "scrapy",
-#         "crawl",
-#         "booksearch",
-#         "-akeyword=斯蒂芬金",
-#         "-apage=1",
-#         "-sMONGO_URL=mongodb://localhost:27017/",
-#         "-sSPIDER_NAME=booksearch",
-#         "-sTASK_ID=001",
-#         "-sJOB_ID=002",
-#     ]
-# )
+execute(
+    [
+        "scrapy",
+        "crawl",
+        "booksearch",
+        "-akeyword=刘慈欣",
+        "-apage=1",
+        "-sMONGO_URL=mongodb://localhost:27017/",
+        "-sSPIDER_NAME=booksearch",
+        "-sTASK_ID=001",
+        "-sJOB_ID=002",
+    ]
+)
